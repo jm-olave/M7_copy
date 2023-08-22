8d62569fd769a7c36de20d3ee033f6f049978f5e
jm-olave <jm.olave@uniandes.edu.co>
2023-08-22 16:55:25 -0500

    try

---
+++ tienda_musica/tienda_gestion/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+admin.site.register(Artista)
+admin.site.register(Album)
+admin.site.register(Genero)
+admin.site.register(AlbumArte)
+admin.site.register(Cancion)
